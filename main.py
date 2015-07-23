fb7184ae4071dbd67a6e88c684dceccfce635099
generalbrae <pedrocowman@gmail.com>
2015-07-23 10:10:13 +0100

    Significant code refactoring and increased robustness of server

--- main.py
+++ main.py
@@ -2,163 +2,265 @@
 
 import socket
 import sys
+import time
+import datetime
 
-def parseHelo(data, logfile):
-    logfile.write("C: " + data + "\n")
-    heloSplit = data.split()
-    if heloSplit[0] == "HELO":
-        if heloSplit[1] == "SMTP.BRAE.IO":
-            print "HELO response OK"
-            client.send("250 SMTP.BRAE.IO\n\n")
-            logfile.write("S: 250 SMTP.BRAE.IO\n\n")
-            return 1
-        else:
-            print "HELO response incorrect"
-            client.send("501 HELO Invalid domain address\n")
-            logfile.write("S: 501 HELO Invalid domain address\n")
-            return 0
-    else:
-        print "Not a HELO response"
-        client.send("502 Use HELO/EHLO first\n")
-        logfile.write("S: 502 Use HELO/EHLO first\n")
-        return 0
+class Logger:
 
-def parseMailFrom(data, logfile):
-    logfile.write("C: " + data + "\n")
-    mailSplit = data.split()
-    if mailSplit[0] == "MAIL":
-        mailSplitb = mailSplit[1].split(":")
-        if mailSplitb[0] == "FROM":
-            mailSplitc = mailSplitb[1].split("@")
-            if mailSplitc[1] == "BRAE.IO>":
-                print "Mail command OK"
-                client.send("250 OK\n\n")
-                logfile.write("S: 250 OK\n\n")
+    logFile = None
+    def __init__(self, fname):
+        try:
+            Logger.logfile = open(fname, 'w')
+        except IOError:
+            print "[!] Error opening log file. Quitting..."
+            sys.exit(1)
+
+
+    def write_to_log(self, data, id):
+        self.ts = time.time()
+        self.stamp = datetime.datetime.fromtimestamp(self.ts).strftime('%d-%m-%Y %H:%M:%S')
+        Logger.logfile.write(self.stamp + " - " + id + ": " + data + "\n")
+
+    def close_log(self):
+        Logger.logfile.close()
+
+class ConnectionHandler:
+
+    _Client = None
+    _Logger = None
+
+    # initialise the handler
+    def __init__(self, client, logger):
+        ConnectionHandler._Client = client
+        ConnectionHandler._Logger = logger
+        self.stc = self.sendToClient("220 SMTP.BRAE.IO Simple Mail Transfer Service Ready\n")
+        if not self.stc:
+            return
+        ConnectionHandler._Logger.write_to_log("220 SMTP.BRAE.IO Simple Mail Transfer Service Ready", "S")
+
+        #Handle HELO
+        self.response = ConnectionHandler._Client.recv(1024)
+        ConnectionHandler._Logger.write_to_log(self.response, "C")
+        while self.response[:4] != "HELO":
+            self.stc = self.sendToClient("502 Use HELO/EHLO first\n")
+            if not self.stc:
+                return
+            ConnectionHandler._Logger.write_to_log("502 Use HELO/EHLO first","S")
+            self.response = ConnectionHandler._Client.recv(1024)
+
+        while True:
+            self.parseSuccess = self.parseHelo(self.response)
+            if self.parseSuccess == 1:
+                break
+            else:
+                self.response = ConnectionHandler._Client.recv(1024)
+                ConnectionHandler._Logger.write_to_log(self.response, "C")
+
+
+        #Handle MAIL FROM
+        self.response = ConnectionHandler._Client.recv(1024)
+        ConnectionHandler._Logger.write_to_log(self.response, "C")
+        while self.response[:4] !=  "MAIL":
+            self.stc = self.sendToClient("550 Invalid syntax. Syntax should be MAIL FROM:<userdomain>\n")
+            if not self.stc:
+                return
+            ConnectionHandler._Logger.write_to_log("550 Invalid syntax. Syntax should be MAIL FROM:<userdomain>", "S")
+            self.response = ConnectionHandler._Client.recv(1024)
+
+        while True:
+            self.parseSuccess = self.parseMailFrom(self.response)
+            if self.parseSuccess == 1:
+                break
+            else:
+                self.response = ConnectionHandler._Client.recv(1024)
+                ConnectionHandler._Logger.write_to_log(self.response, "C")
+
+        #Handle recipient(s)
+        self.response = ConnectionHandler._Client.recv(1024)
+        ConnectionHandler._Logger.write_to_log(self.response, "C")
+        while self.response[:4] != "RCPT":
+            self.stc = self.sendToClient("550 Invalid syntax. Syntax should be RCPT TO:<recipientaddress\n")
+            if not self.stc:
+                return
+            ConnectionHandler._Logger.write_to_log("550 Invalid syntax. Syntax should be RCPT TO:<recipientaddress", "S")
+            self.response = ConnectionHandler._Client.recv(1024)
+
+        while self.response[:4] == "RCPT": #For multiple recipientaddress
+            while True:
+                self.parseSuccess = self.parseRcpt(self.response)
+                if self.parseSuccess == 1:
+                    break
+                else:
+                    self.response = ConnectionHandler._Client.recv(1024)
+                    ConnectionHandler._Logger.write_to_log(self.response, "C")
+            self.response = ConnectionHandler._Client.recv(1024)
+            ConnectionHandler._Logger.write_to_log(self.response, "C")
+
+        #Handle data
+        while self.response[:4] != "DATA":
+            self.stc = self.sendToClient("550 Unexpected command\n")
+            if not self.stc:
+                return
+            ConnectionHandler._Logger.write_to_log("550 Unexpected command", "S")
+            self.response = ConnectionHandler._Client.recv(1024)
+
+        self.dataGet = self.getData()
+
+        self.response = ConnectionHandler._Client.recv(1024)
+        ConnectionHandler._Logger.write_to_log(self.response, "C")
+
+        while self.response[:4] != "QUIT":
+            self.stc = self.sendToClient("550 Unexpected command\n")
+            if not self.stc:
+                return
+            ConnectionHandler._Logger.write_to_log("550 Unexpected command", "S")
+            self.response = ConnectionHandler._Client.recv(1024)
+
+        print "[!] Quit received"
+        self.stc = self.sendToClient("221 SMTP.BRAE.IO Service closing transmission channel\n")
+        if not self.stc:
+            return
+        ConnectionHandler._Logger.write_to_log("221 SMTP.BRAE.IO Service closing transmission channel", "S")
+        ConnectionHandler._Logger.write_to_log("\n=========================================================\n", "S")
+        ConnectionHandler._Client.close()
+
+    def sendToClient(self, data):
+        try:
+            ConnectionHandler._Client.send(data)
+        except socket.error, v:
+            print "Connection lost"
+            ConnectionHandler._Logger.write_to_log("Connection lost", "Error")
+            return False
+        return True
+
+
+    def getData(self):
+        self.stc = self.sendToClient("354 Start mail input; end with <CRLF>.<CRLF>\n")
+        if not self.stc:
+            return
+        ConnectionHandler._Logger.write_to_log("354 Start mail input; end with <CRLF>.<CRLF>", "S")
+        self.content = ""
+        while True:
+            self.line = ConnectionHandler._Client.recv(1024)
+            if self.line and self.line[:1] == ".":
+                print "[i] Message complete:", self.content
+                self.stc = self.sendToClient("250 OK\n\n")
+                if not self.stc:
+                    return
+                ConnectionHandler._Logger.write_to_log(self.content + "\n", "C")
                 return 1
             else:
-                print mailSplitc[1], "\n"
-                print "Invalid mail domain"
-                client.send("550 The address is not valid\n")
-                logfile.write("S: 550 The address is not valid\n")
+                self.content += self.line
+                print self.line
+
+
+    def parseHelo(self, data):
+        if len(data) > 4:
+            self.heloSplit = data.split(' ')
+            if self.heloSplit[0] and self.heloSplit[0] == "HELO":
+                if self.heloSplit[1] and self.heloSplit[1].rstrip() == "SMTP.BRAE.IO":
+                    print "[i] HELO response OK"
+                    self.stc = self.sendToClient("250 SMTP.BRAE.IO\n\n")
+                    if not self.stc:
+                        return
+                    ConnectionHandler._Logger.write_to_log("250 SMTP.BRAE.IO\n", "S")
+                    return 1
+                else:
+                    print "[i] HELO domain incorrect"
+                    self.stc = self.sendToClient("501 HELO Invalid domain address\n")
+                    if not self.stc:
+                        return
+                    ConnectionHandler._Logger.write_to_log("501 HELO Invalid domain address", "S")
+                    return 0
+            else:
+                print "[i] Not a HELO response"
+                self.stc = self.sendToClient("502 Use HELO/EHLO first\n")
+                if not self.stc:
+                    return
+                ConnectionHandler._Logger.write_to_log("502 Use HELO/EHLO first", "S")
+                return 0
+
+    def parseMailFrom(self, data):
+        self.mailSplit = data.split()
+        if self.mailSplit[0] and self.mailSplit[1] and self.mailSplit[0] == "MAIL":
+            self.mailSplitb = self.mailSplit[1].split(":")
+            if self.mailSplitb[0] and self.mailSplitb[1] and self.mailSplitb[0] == "FROM":
+                self.mailSplitc = self.mailSplitb[1].split("@")
+                if self.mailSplitc[1] and self.mailSplitc[1] == "BRAE.IO>":
+                    print "[i] Mail command OK"
+                    self.stc = self.sendToClient("250 OK\n\n")
+                    if not self.stc:
+                        return
+                    ConnectionHandler._Logger.write_to_log("S: 250 OK\n", "S")
+                    return 1
+                else:
+                    print "[i] Invalid mail domain"
+                    self.stc = self.sendToClient("550 The address is not valid\n")
+                    if not self.stc:
+                        return
+                    ConnectionHandler._Logger.write_to_log("550 The address is not valid", "S")
+                    return 0
+            else:
+                print "[i] Invalid mail syntax"
+                self.stc = self.sendToClient("550 Invalid syntax. Syntax should be MAIL FROM:<userdomain>\n")
+                if not self.stc:
+                    return
+                ConnectionHandler._Logger.write_to_log("550 Invalid syntax. Syntax should be MAIL FROM:<userdomain>", "S")
                 return 0
         else:
-            print "Invalid syntax"
-            client.send("550 Invalid syntax. Syntax should be MAIL FROM:<userdomain>\n")
-            logfile.write("S: 550 Invalid syntax. Syntax should be MAIL FROM:<userdomain>\n")
+            print "[i] Invalid mail command"
+            self.stc = self.sendToClient("550 Invalid syntax. Syntax should be MAIL FROM:<userdomain>\n")
+            if not self.stc:
+                return
+            ConnectionHandler._Logger.write_to_log("550 Invalid syntax. Syntax should be MAIL FROM:<userdomain>", "S")
             return 0
-    else:
-        print "Invalid command"
-        client.send("550 Invalid syntax. Syntax should be MAIL FROM:<userdomain>\n")
-        logfile.write("S: 550 Invalid syntax. Syntax should be MAIL FROM:<userdomain>\n")
-        return 0
 
-def parseRcpt(data, logfile):
-    logfile.write("C: " + data + "\n")
-    rcptSplit = data.split()
-    if rcptSplit[0] == "RCPT":
-        rcptSplitb = rcptSplit[1].split(":")
-        if rcptSplitb[0] == "TO":
-            print "RCPT command OK"
-            client.send("250 OK\n\n")
-            logfile.write("S: 250 OK\n\n")
-            return 1
+    def parseRcpt(self, data):
+        self.rcptSplit = data.split()
+        if self.rcptSplit[0] and self.rcptSplit[0] == "RCPT":
+            self.rcptSplitb = self.rcptSplit[1].split(":")
+            if self.rcptSplitb[0] and self.rcptSplitb[0] == "TO":
+                print "[i] RCPT command OK"
+                self.stc = self.sendToClient("250 OK\n\n")
+                if not self.stc:
+                    return
+                ConnectionHandler._Logger.write_to_log("250 OK\n", "S")
+                return 1
+            else:
+                print "[i] RCPT syntax incorrect"
+                self.stc = self.sendToClient("550 Invalid syntax. Syntax should be RCPT TO:<recipientaddress>\n")
+                if not self.stc:
+                    return
+                ConnectionHandler._Logger.write_to_log("550 Invalid syntax. Syntax should be RCPT TO:<recipientaddress>", "S")
+                return 0
         else:
-            print "RCPT syntax incorrect"
-            client.send("550 Invalid syntax. Syntax should be RCPT TO:<recipientaddress>\n")
-            logfile.write("S: 550 Invalid syntax. Syntax should be RCPT TO:<recipientaddress>\n")
+            print "[i] RCPT syntax incorrect"
+            self.stc = self.sendToClient("550 Invalid syntax. Syntax should be RCPT TO:<recipientaddress\n")
+            if not self.stc:
+                return
+            ConnectionHandler._Logger.write_to_log("550 Invalid syntax. Syntax should be RCPT TO:<recipientaddress>", "S")
             return 0
-    else:
-        print "RCPT syntax incorrect"
-        client.send("550 Invalid syntax. Syntax should be RCPT TO:<recipientaddress\n")
-        logfile.write("S: 550 Invalid syntax. Syntax should be RCPT TO:<recipientaddress>\n")
-        return 0
-
-def getData(logfile):
-    client.send("354 Start mail input; end with <CRLF>.<CRLF>\n")
-    logfile.write("S: 354 Start mail input; end with <CRLF>.<CRLF>\n")
-    content = ""
-    while True:
-        line = client.recv(1024)
-        if line[:1] == ".":
-            print "Message complete:", content
-            client.send("250 OK\n\n")
-            logfile.write("C: " + content + "\n\n")
-            return content
-        else:
-            content += line
-            print line
 
 
 
 
-def clientHandler(client, logfile):
-    client.send("220 SMTP.BRAE.IO Simple Mail Transfer Service Ready\n")
-    logfile.write("S: 220 SMTP.BRAE.IO Simple Mail Transfer Service Ready\n")
-    response = client.recv(1024)
 
-    #Loop until correct input given i.e. HELO SMTP.BRAE.IO
-    while True:
-        success = parseHelo(response, logfile)
-        if success == 0:
-            response = client.recv(1024)
-        else:
-            break
-
-    #Check the MAIL FROM command that should come next
-    response = client.recv(1024)
-    while True:
-        success = parseMailFrom(response, logfile)
-        if success == 0:
-            response = client.recv(1024)
-        else:
-            break
-
-    #Handle RCPT command(s)
-    response = client.recv(1024)
-    while True:
-        success = parseRcpt(response, logfile)
-        if success == 0:
-            response = client.recv(1024)
-        else:
-            response = client.recv(1024)
-            if response[:4] != "RCPT":
-                break
-
-    #Handle data
-    while True:
-        if response[:4] == "DATA":
-            logfile.write("C: DATA\n")
-            data = getData(logfile)
-            break
-        else:
-            client.send("550 Unexpected command\n")
-            response = client.recv(1024)
-
-    #Handle quit
-    while True:
-        response = client.recv(1024)
-        if response[:4] == "QUIT":
-            print "QUIT received"
-            logfile.write("C: QUIT\n")
-            client.send("221 SMTP.BRAE.IO Service closing transmission channel\n")
-            logfile.write("S: 221 SMTP.BRAE.IO Service closing transmission channel\n")
-            break
-        else:
-            client.send("550 Unexpected command\n")
-            logfile.write("S: 550 Unexpected command\n")
-    client.close()
-    logfile.write("\n\n---------------------------------------------------------------------\n\n")
-
+print "Launching SMTP Honeypot\n=======================\n\n"
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-print "Creating socket..."
-print "Binding..."
-sock.bind(('127.0.0.1', 25))
-print "Opening log file"
-logFile = open("honey.log", 'a')
+print "[i] Socket initialised"
+try:
+    sock.bind(('172.16.0.201', 25))
+except socket.error, v:
+    print "[!] Socket binding failed. Exiting..."
+    sys.exit(1)
+print "[i] Socket bound"
+honeyLog = Logger("robusthoney.log")
+print "[i] Log file opened"
 sock.listen(5)
-print "Listening on port 25"
+print "[i] Listening on port 25..."
 while True:
-    client, addr = sock.accept()
-    print "Received connection"
-    clientHandler(client,logFile)
+    c, addr = sock.accept()
+    print "[i] Connection attempt received. Passing to handler..."
+    clientH = ConnectionHandler(c, honeyLog)
+    print "[i] Connection complete. Listening..."
